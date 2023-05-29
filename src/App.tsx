5c29344a5f6215b23e49a082d90d429e0453e9e2
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-05-29 08:37:44 +0700

    day01

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
