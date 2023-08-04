d8afc18a5ee12eb757b7d636c05c18c6b23fcdcc
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2023-08-04 11:25:41 +0800

    Debug sederhana react native

--- src/pages/Akun/index.js
+++ src/pages/Akun/index.js
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
+import axios from 'axios'
+// Your API key is: 3bb8584f64c9477cb81cc4371fb68336
+// https://newsapi.org/v2/top-headlines
+
+const buttonFunction =()=>{
+    console.log('testtt')
+}
 
 const Akun = () => {
   return (
     <View>
+      <TouchableOpacity onPress={()=>buttonFunction()}>
       <Text>Akun</Text>
+      </TouchableOpacity>
     </View>
   )
 }
